63fd58b3075a5fb51d404b10bbd67a6f9ce405fc
thegrayfoxxx <cool.afkshnik@gmail.com>
2025-04-19 02:02:18 +0300

    Switch fq to fq_codel for qdisc Fix correct uninstall bbr

--- scripts/bbr.py
+++ scripts/bbr.py
@@ -6,7 +6,7 @@ class BBR:
     def __init__(self):
         self.path_to_sysctl_config = "/etc/sysctl.conf"
         self.bbr_config = """
-net.core.default_qdisc=fq
+net.core.default_qdisc=fq_codel
 net.ipv4.tcp_congestion_control=bbr
 """
 
@@ -17,6 +17,7 @@ net.ipv4.tcp_congestion_control=bbr
         subprocess.run(["sysctl", "-p"])
         time.sleep(1)
         subprocess.run(["sysctl", "net.ipv4.tcp_congestion_control"])
+        subprocess.run(["sysctl", "net.core.default_qdisc"])
 
     def uninstall_bbr(self):
         with open(self.path_to_sysctl_config, "r") as f:
@@ -28,7 +29,10 @@ net.ipv4.tcp_congestion_control=bbr
                     lines.remove(line)
             with open(self.path_to_sysctl_config, "w") as f:
                 f.writelines(lines)
-
+        subprocess.run(["sysctl", "net.ipv4.tcp_congestion_control=cubic"])
+        subprocess.run(["sysctl", "net.core.default_qdisc=fq_codel"])
+        subprocess.run(["sysctl", "net.ipv4.tcp_congestion_control"])
+        subprocess.run(["sysctl", "net.core.default_qdisc"])
 
 if __name__ == "__main__":
     user_input = input("Exit - 0\nInstall - 1\nUninstall - 2\n")
