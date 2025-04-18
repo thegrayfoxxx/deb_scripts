d82c27cf7a01649511b1bec11cbd73f6b4b50c4b
thegrayfoxxx <cool.afkshnik@gmail.com>
2025-04-19 01:20:41 +0300

    Refactoring bbr install to OOP and add interactive run

--- scripts/bbr_install.py
+++ scripts/bbr_install.py
@@ -1,17 +1,42 @@
 import subprocess
 import time
 
-bbr_config = """
+
+class BBR:
+    def __init__(self):
+        self.path_to_sysctl_config = "/etc/sysctl.conf"
+        self.bbr_config = """
 net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr
 """
 
-path = "/etc/sysctl.conf"
+    def install_bbr(self):
+        with open(self.path_to_sysctl_config, "a") as f:
+            f.write(self.bbr_config)
+
+        subprocess.run(["sysctl", "-p"])
+        time.sleep(1)
+        subprocess.run(["sysctl", "net.ipv4.tcp_congestion_control"])
+
+    def uninstall_bbr(self):
+        with open(self.path_to_sysctl_config, "r") as f:
+            lines = f.readlines()
+            new_lines = [
+                line
+                for line in lines
+                if not line.startswith("net.ipv4.tcp_congestion_control")
+            ]
+            with open(self.path_to_sysctl_config, "w") as f:
+                f.writelines(new_lines)
 
 
-with open(path, "a") as f:
-    f.write(bbr_config)
-
-subprocess.run(["sysctl", "-p"])
-time.sleep(1)
-subprocess.run(["sysctl", "net.ipv4.tcp_congestion_control"])
+if __name__ == "__main__":
+    user_input = input("Exit - 0\nInstall - 1\nUninstall - 2\n")
+    bbr = BBR()
+    match user_input:
+        case "0":
+            exit(0)
+        case "1":
+            bbr.install_bbr()
+        case "2":
+            bbr.uninstall_bbr()
