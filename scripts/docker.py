7bd3d881083e21589ba39458bd6d3b3a973a9e79
anatoliy <cool.afkshnik@gmail.com>
2025-09-10 00:50:41 +0300

    Add Docker installation script and CLI interface

---
+++ scripts/docker.py
@@ -0,0 +1,30 @@
+from utils.subprocess_utils import run_commands
+
+
+class Docker:
+    def install_docker(self):
+        commands = [
+            ["apt", "update"],
+            ["apt", "install", "-y", "curl"],
+            ["curl", "-fsSL", "https://get.docker.com", "-o", "get-docker.sh"],
+            ["sh", "./get-docker.sh"],
+            ["rm", "./get-docker.sh"],
+        ]
+        run_commands(commands)
+
+    def interactive_run(self):
+        print("Docker install")
+        user_input = str(input("Exit - 0\nInstall - 1\n"))
+        match user_input:
+            case "0":
+                print("Exiting...")
+            case "1":
+                self.install_docker()
+            case _:
+                print("Invalid input")
+                self.interactive_run()
+
+
+if __name__ == "__main__":
+    docker = Docker()
+    docker.interactive_run()
