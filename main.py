9564d163b8ac56ef2f76ce7223ec9d237246c7cb
anatoliy <cool.afkshnik@gmail.com>
2025-09-14 22:43:27 +0300

    Move interactive script logic to separate module

--- main.py
+++ main.py
@@ -1,30 +1,8 @@
-from scripts.bbr import BBR
-from scripts.docker import Docker
-from scripts.fail2ban import Fail2Ban
-from scripts.uv import UV
+from scripts.run import run_interactive_script
 
 
 def main():
-    print("Hello from deb-scripts!")
-    user_input = str(input("Exit - 0\nBBR - 1\nDocker - 2\nFail2Ban - 3\nUV - 4\n"))
-    match user_input:
-        case "0":
-            exit()
-        case "1":
-            bbr = BBR()
-            bbr.interactive_run()
-        case "2":
-            docker = Docker()
-            docker.interactive_run()
-        case "3":
-            fail2ban = Fail2Ban()
-            fail2ban.interactive_run()
-        case "4":
-            uv = UV()
-            uv.interactive_run()
-        case _:
-            print("Invalid input")
-            main()
+    run_interactive_script()
 
 
 if __name__ == "__main__":
---
+++ scripts/run.py
@@ -0,0 +1,27 @@
+from scripts.bbr import BBR
+from scripts.docker import Docker
+from scripts.fail2ban import Fail2Ban
+from scripts.uv import UV
+
+
+def run_interactive_script():
+    print("Hello from deb-scripts!")
+    user_input = str(input("Exit - 0\nBBR - 1\nDocker - 2\nFail2Ban - 3\nUV - 4\n"))
+    match user_input:
+        case "0":
+            exit()
+        case "1":
+            bbr = BBR()
+            bbr.interactive_run()
+        case "2":
+            docker = Docker()
+            docker.interactive_run()
+        case "3":
+            fail2ban = Fail2Ban()
+            fail2ban.interactive_run()
+        case "4":
+            uv = UV()
+            uv.interactive_run()
+        case _:
+            print("Invalid input")
+            run_interactive_script()
