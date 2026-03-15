472ece30a5d1723693122653d74150d82f918d96
anatoliy <cool.afkshnik@gmail.com>
2026-03-15 06:54:11 +0300

    feat: add root permission check and logging

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from app.interfaces.interactive.run import run_interactive_script
+from app.utils.logger import get_logger
+from app.utils.permission_utils import check_root
+
+logger = get_logger(__name__)
 
 
 def main():
+    check_root()
     run_interactive_script()
 
 
@@ -9,4 +14,8 @@ if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        print("Выход...")
+        logger.info("Выход...")
+    except PermissionError:
+        logger.error(
+            "Недостаточно прав для выполнения операции. Запустите скрипт с правами суперпользователя. (sudo)"
+        )
