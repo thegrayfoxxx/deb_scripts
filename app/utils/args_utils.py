34009886b73688235eb53efba2956e309664767b
anatoliy <cool.afkshnik@gmail.com>
2026-03-22 05:04:15 +0300

    refactor: conditionally parse args for testability

--- app/utils/args_utils.py
+++ app/utils/args_utils.py
@@ -1,4 +1,5 @@
 import argparse
+import sys
 
 
 def parse_args():
@@ -13,4 +14,18 @@ def parse_args():
     return parser.parse_args()
 
 
-app_args = parse_args()
+# Don't automatically parse args when running in pytest
+if "pytest" in sys.modules:
+    # During tests, provide a default configuration
+    class DefaultArgs:
+        mode = "prod"
+
+    app_args = DefaultArgs()
+else:
+    # In normal execution, parse command line arguments
+    app_args = parse_args()
+
+
+def get_app_args():
+    """Get the application arguments (for use in tests)"""
+    return app_args
