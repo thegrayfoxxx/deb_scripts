60d5c63f5fad47c3d28fea63a2121e75507f965f
anatoliy <cool.afkshnik@gmail.com>
2026-03-26 01:51:50 +0300

    feat: Add non-interactive command handling

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from app.interfaces.cli.non_interactive import run_non_interactive_commands
 from app.interfaces.interactive.run import run_interactive_script
+from app.utils.args_utils import app_args
 from app.utils.logger import get_logger
 from app.utils.permission_utils import check_root
 
@@ -7,15 +9,20 @@ logger = get_logger(__name__)
 
 def main():
     check_root()
-    run_interactive_script()
+
+    # Handle non-interactive commands
+    if app_args.install or app_args.uninstall:
+        run_non_interactive_commands(app_args)
+    else:
+        run_interactive_script()
 
 
 if __name__ == "__main__":
     try:
         main()
     except KeyboardInterrupt:
-        logger.info("Выход...")
+        logger.info("👋 Выход...")
     except PermissionError:
         logger.error(
-            "Недостаточно прав для выполнения операции. Запустите скрипт с правами суперпользователя. (sudo)"
+            "🔐 Недостаточно прав для выполнения операции. Запустите скрипт с правами суперпользователя. (sudo)"
         )
